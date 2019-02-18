20eae72a12c48a5a1c2b518b4196d16f69e90f8a
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-02-18 14:42:52 -0500

    fixed hamburger menu and hero

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,32 @@
-"use strict";var hamburger=document.querySelector(".hamburger");hamburger.addEventListener("click",function(){var e=document.querySelector(".overlay");hamburger.classList.toggle("active"),e.classList.toggle("open")});
+"use strict";
+
+var hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function () {
+    document.body.classList.toggle("hamburger_open");
+});
+
+window.addEventListener("load", function () {
+    TweenMax.staggerFromTo([".first-word-h2", ".second-word-h2", ".third-word-h2", ".hero-span"], 1, {
+        y: 50,
+        opacity: 0
+    }, {
+        opacity: 1,
+        y: 0,
+        delay: 1
+    }, 0.2);
+});
+
+// var parallaxInstance = new Parallax(document.querySelector('.hero-text'));
+
+
+window.addEventListener("mousemove", function (e) {
+    console.log(e);
+
+    var mouseX = e.clientX / window.innerWidth;
+
+    TweenMax.to([".first-word-h2", ".second-word-h2", ".third-word-h2"], 1, {
+        x: mouseX * -20
+    });
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,41 @@
 let hamburger = document.querySelector(`.hamburger`)
 
 hamburger.addEventListener(`click`, function(){
-    let overlayMenu = document.querySelector(`.overlay`)
-    hamburger.classList.toggle(`active`)
-    overlayMenu.classList.toggle(`open`)
+    document.body.classList.toggle(`hamburger_open`)
+})
+
+
+
+window.addEventListener("load", function() {
+    TweenMax.staggerFromTo([
+        ".first-word-h2", 
+        ".second-word-h2", 
+        ".third-word-h2",
+        ".hero-span"
+    ], 1, {
+        y: 50,
+        opacity: 0,
+    }, {
+        opacity: 1,
+        y: 0,
+        delay: 1
+    }, 0.2)
+
+})
+
+// var parallaxInstance = new Parallax(document.querySelector('.hero-text'));
+
+
+window.addEventListener("mousemove", function(e) {
+    console.log(e);
+
+    let mouseX = e.clientX / window.innerWidth;
+
+    TweenMax.to([
+        ".first-word-h2", 
+        ".second-word-h2", 
+        ".third-word-h2"
+    ], 1,  {
+        x: mouseX * -20,
+    })
 })
